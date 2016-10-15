1925e5a3267ef3841cb66e0d06cbe56d521cc7b1
SBRhen <sbrhen@yandex.ru>
2016-10-15 19:09:51 +0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,27 @@
+import os
+import socket
+
+_socket = socket.socket()
+_socket.bind(('localhost',8000))
+_socket.listen(1)
+
+while True:
+    connection, address = _socket.accept()
+    data = connection.recv(1024)
+    request = data.decode("utf-8")
+    print(data)
+    address = data.split(' ')[1]
+
+    if address == "/index.html" or address == "/":
+        if os.path.exists("./index.html"):
+            file = open("index.html",'r')
+            connection.send("""HTTP/1.1 200 OK\n"+
+            "Content-type: text HTML\n\n""" + file.read())
+            file.close()
+    elif address == "/about/aboutme.html":
+        if os.path.exists("./about/aboutme.html"):
+            file = open("about/aboutme.html",'r')
+            connection.send("""HTTP/1.1 200 OK\n"+"Content-type: text HTML\n\n""" + file.read())
+            file.close()    
+    connection.close()
+_socket.close()
